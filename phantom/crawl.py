6996851241eb4416d148479f535326e22a3869ee
fafnir <gokurohono@gmail.com>
2024-08-03 19:27:52 +1000

    feat: adding page substring logic, is page an asset logic

--- phantom/crawl.py
+++ phantom/crawl.py
@@ -7,7 +7,7 @@ import time
 from rapidfuzz import fuzz, process
 
 
-class Crawl(BaseModel):
+class Crawl:
     """given a base Page start crawling using a BFS algorithm."""
 
     start: Page
@@ -20,23 +20,24 @@ class Crawl(BaseModel):
 
         self.queue.append(self.start)
 
-    def start(self, timeout: float = 1, max_depth=1):
+    def run(self, timeout: float = 1, max_depth=1):
         """Starts the crawling process."""
         curr_depth = 0
         while len(self.queue) > 0 and curr_depth < max_depth:
             _curr = self.queue.pop(0)
+            page_domain = _curr.get_domain()
 
+            
             soup = parse_contents(_curr.url)
 
             for link in soup.find_all("a", href=True):
                 # if internal link "process it accordingly"
-                print(link)
+                # print(link)
                 # calculate similarity
-
                 #
 
             time.sleep(timeout)
 
 
 if __name__ == "__main__":
-    Crawl(Page("https://docs.pydantic.dev/latest/#pydantic-examples")).start()
+    Crawl(start=Page(url="https://docs.pydantic.dev/latest/#pydantic-examples")).run()
--- phantom/model.py
+++ phantom/model.py
@@ -2,6 +2,7 @@
 
 from __future__ import annotations
 from pydantic import BaseModel
+import re
 
 
 class Page(BaseModel):
@@ -12,6 +13,16 @@ class Page(BaseModel):
     """
 
     url: str
+
+    def get_domain(self) -> str:
+        """given a url is https://{domain}/..."""
+        pattern = r"https?://([a-zA-Z0-9.-]+\.[a-zA-Z]{2,})"
+        return re.search(pattern, self.url).group(0)
+
+    def is_page_an_asset(self) -> bool:
+        """returns if the current page is an asset rather than a html website"""
+        pattern = r"(.+)\.(png|svg|jpeg|pdf|csv|xlsx)"
+        return True if re.match(pattern, self.url) else False
 
 
 class Link(BaseModel):
